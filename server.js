04924969b7605b400019ef58b611915ff8c15359
Sumit Khandelwal <sumit.khandelwal@mindtree.com>
2020-12-14 16:05:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const axios = require("axios").default;
+
+const users = [
+    {email : "test@test.com", age : 32},
+    {email : "test1@test.com", age : 33}
+]
+app.get("/api/users", (req, res) => {
+    res.send(users);
+})
+
+app.get("/todo", (request, response) => {
+    response.send({message : "SUCCESS"});
+})
+
+app.get("/demo", (req, res) => {    
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
+app.get("/some-action", (req, res) => {
+    axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/delhi.json?access_token=pk.eyJ1Ijoic3luZXJneTI0MTEiLCJhIjoiY2p4NXc0cm53MDZoODQwbHFuNzdzMzV5NCJ9.DKIDo6bcG51yLXf2DmlYcQ")
+        .then(response => console.log(response));
+    res.send({message : "Form Submitted"});
+})
+
+app.listen(9000, () => {
+    console.log("Express Server running on Port 9000...");
+})
